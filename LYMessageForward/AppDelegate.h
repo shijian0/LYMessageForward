a799be0bfc1283317a9b2160e543d5a6a0788541
liyong <liyong@xnw.com>
2019-04-08 13:00:02 +0800

    Initial Commit

---
+++ LYMessageForward/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYMessageForward
+//
+//  Created by LiYong on 2019/4/8.
+//  Copyright © 2019 勇 李. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYMessageForward/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYMessageForward
+//
+//  Created by LiYong on 2019/4/8.
+//  Copyright © 2019 勇 李. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
